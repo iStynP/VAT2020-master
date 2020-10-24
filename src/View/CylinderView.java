c67117b54d4f4fa0530f6b01f96895fa79ef94ca
iStyn <45605792+iStynP@users.noreply.github.com>
2020-10-24 15:44:42 +0200

    Create CylinderView.java

---
+++ src/View/CylinderView.java
@@ -0,0 +1,34 @@
+package Domain;
+
+import java.text.DecimalFormat;
+import java.util.Objects;
+
+public class TwoDimensionalShape implements Shape {
+    private double surface;
+    private double perimeter;
+    private String name;
+
+    public TwoDimensionalShape(String name) {
+        this.name = name;
+    }
+
+    public double getSurface(){
+        return surface;
+    }
+
+    public double getPerimeter(){
+        return perimeter;
+    }
+
+    public String getNameOfShape() {
+        return this.name;
+    }
+
+    public String toString() {
+        String strSurface = ( new DecimalFormat("##.##").format(getSurface()) );
+        String strPerimeter = ( new DecimalFormat("##.##").format(getPerimeter()) );
+        return "This " + name + " has a surface of " + strSurface + " cm\u00B2\n" +
+                "and a perimeter of " + strPerimeter + " cm";
+    }
+
+}
